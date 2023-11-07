fc7f8a134054f21a8a94f2f00b364b4118d1e6c7
RamyaShanthi-27 <ramyashanthi@gmail.com>
2023-11-07 14:57:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Message from './Message';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <Message></Message>
 
     </div>
   );

9a2bead47fbd20cccd01913f1ff1c5b13af90cb1
RamyaShanthi-27 <ramyashanthi@gmail.com>
2023-11-07 22:56:48 +0530

    u

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Message from './Message';
 
 function App() {
   return 
-    //<div className="App">
-     /* <Message place="Earth" at1="value1"></Message>
+    <div className="App">
+      <Message place="Earth" at1="value1"></Message>
      <Message place="Moon"></Message>
      <Message place="Mars"></Message>
-     <Message place="Jupiter"></Message> */
-     //</div>
-<section class="pricing py-5">
-    <div class="container">
-      <div class="row">
+     <Message place="Jupiter"></Message>
+     </div>
+//<section class="pricing py-5">
+    //<div class="container">
+      //<div class="row">
 
-     <Message plan="Free" price="$0/month" user="Single User" stroage="5GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Report" ></Message>
-     <Message plan="Plus" price="$9/month" user="5 User" stroage="50GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Report" ></Message>
-     <Message plan="PRO" price="$49/month" user="Unlimited User" stroage="150GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Unlimited Free Subdomain" reports="Monthly Status Report"></Message>
+     //<Message plan="Free" price="$0/month" user="Single User" stroage="5GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Report" ></Message>
+    // <Message plan="Plus" price="$9/month" user="5 User" stroage="50GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Report" ></Message>
+    // <Message plan="PRO" price="$49/month" user="Unlimited User" stroage="150GB Sgtorage" pubproject="Unlimited Public Project" access="Community Access" priproject="Unlimited Private Projects" phone="Dedicated Phone Support" subdomain="Unlimited Free Subdomain" reports="Monthly Status Report"></Message>
      
     
 
-    </div>
-  </div>
-</section>   
+   // </div>
+ // </div>
+//</section>   
   
 }
 
